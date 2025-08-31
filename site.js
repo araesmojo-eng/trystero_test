509735c4f07e9125e8b2710bc358dd44296a9cc4
Araesmojo <gabriel.putnam@gmail.com>
2025-08-30 20:29:16 -0700

    Add files via upload

--- site.js
+++ site.js
@@ -138,7 +138,7 @@ function addCursor(id, isSelf) {
   var peer_image = peer_images[ id_long_number % 10 ];
   
   img.src = 'images/' + peer_image;
-  txt.innerText = isSelf ? 'you\n' + id + '\n' + id_letters +  '\n' + id_letter_part + '\n' + id_numbers +  '\n' + id_number_part + '\n' + String(id_long_number) : id.slice(0, 4)
+  txt.innerText = isSelf ? 'you\n' + id.slice(0, 4) : id.slice(0, 4)
   el.appendChild(img)
   el.appendChild(txt)
   canvas.appendChild(el)
